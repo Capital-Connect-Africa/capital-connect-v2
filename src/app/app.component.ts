eab3eed9b62923bef17a0d974b52638ff1135317
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-10 08:41:31 +0300

    fix: fixed tailwind installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'capital-connect-v2';
+  constructor(private primeng: PrimeNG) {
+    this.primeng.theme.set({
+        // preset: Aura,
+            options: {
+                cssLayer: {
+                    name: 'primeng',
+                    order: 'tailwind-base, primeng, tailwind-utilities'
+                }
+            }
+        })
+    }
 }
