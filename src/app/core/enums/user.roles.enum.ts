d77c676aaa567a06ea4a20f397ae30cf7c365b0e
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-11 11:34:39 +0300

    chore: added an enum for the user role

---
+++ src/app/core/enums/user.roles.enum.ts
@@ -0,0 +1,8 @@
+export enum USER_ROLES {
+    ADMIN = "admin",
+    USER = "user",
+    INVESTOR = "investor",
+    ADVISOR = "advisor",
+    CONTACT_PERSON = "contact_person",
+    PARTNER ="partner"
+  }
