4dc3da703576a83aa81725248e2123f891afee29
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-11 11:58:28 +0300

    chore: added auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './features/auth/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        loadChildren: () =>import('./features/auth/auth.routes').then(m =>m.AuthRoutes)
+        
+    }
+];
