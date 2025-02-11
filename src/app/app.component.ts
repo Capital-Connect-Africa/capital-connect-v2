f4d641f9c4183fc5725444c61457a1301e527968
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-11 09:41:57 +0300

    chore: install tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimeNG } from 'primeng/config';
-import {ButtonModule} from 'primeng/button';
-
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(private primeng: PrimeNG) {
-    this.primeng.theme.set({
-        // preset: Aura,
-            options: {
-                cssLayer: {
-                    name: 'primeng',
-                    order: 'tailwind-base, primeng, tailwind-utilities'
-                }
-            }
-        })
-    }
+
 }
