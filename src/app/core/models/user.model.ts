5947198594f5b07e6d5be52c14bbf34e2003c2cd
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-11 11:37:49 +0300

    chore: created a user model

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,20 @@
+import { USER_ROLES } from "../enums/user.roles.enum";
+
+export interface User{
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    roles: USER_ROLES[];
+    referralCode: string;
+    resetPasswordToken: string;
+    resetPasswordExpires: Date;
+    isEmailVerified: boolean;
+    emailVerificationToken: string;
+    emailVerificationExpires: string;
+    hasAcceptedTerms: boolean
+    termsAcceptedAt: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}
