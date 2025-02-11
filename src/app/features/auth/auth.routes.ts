c267d953b7bd4220ebe335a78f018f491ce909c8
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-11 12:04:17 +0300

    added signup

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/signup/signup.component').then(c => c.SignupComponent)},
+  { path: 'signup', loadComponent: () => import('./pages/signup/signup.component').then(c => c.SignupComponent)},
 ];
 
 @NgModule({
