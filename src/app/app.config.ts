b12a1fa374e7adb8ed0d39609e31f39732e637c2
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-15 15:35:52 +0300

    chore: added configs for prime ng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,27 @@
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 import { routes } from './app.routes';
+import { FilterMatchMode } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Lara,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'light',
+          cssLayer: false,
+        },
+      },
+    }),
   ],
 };
