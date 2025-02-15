1422a34b49e9b4bb524314d694425df83358c93c
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-15 17:02:41 +0300

    refactor: created a standalone carousel component

--- src/app/features/auth/layout/auth.layout.component.ts
+++ src/app/features/auth/layout/auth.layout.component.ts
@@ -1,31 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CarouselModule } from 'primeng/carousel';
+import { SliderComponent } from "../../../core/components/slider/slider.component";
 
 @Component({
   selector: 'auth-layout',
-  imports: [RouterOutlet, CarouselModule],
+  imports: [RouterOutlet, SliderComponent],
   templateUrl: './auth.layout.component.html',
   styleUrl: './auth.layout.component.scss'
 })
 export class AuthLayoutComponent {
 
-  slides =[
-    {
-      image: 'assets/img/businessman-working-laptop 1.png',
-      quote: 'It simplifies collaboration and keeps everyone on the same page, allowing us to focus more on creativity and less on logistics.',
-      author: 'Robert Howard - Project Manager'
-    },
-    {
-      image: 'assets/img/happyman.png',
-      quote: 'It simplifies collaboration and keeps everyone on the same page, allowing us to focus more on creativity and less on logistics.',
-      author: 'Robert Howard - Project Manager'
-    },
-    {
-      image: 'assets/img/happy-team.png',
-      quote: 'It simplifies collaboration and keeps everyone on the same page, allowing us to focus more on creativity and less on logistics.',
-      author: 'Robert Howard - Project Manager'
-    }
-
-  ]
 }
