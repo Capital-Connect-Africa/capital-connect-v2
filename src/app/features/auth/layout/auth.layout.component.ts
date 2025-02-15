99e417bb147f5fd9881f0cd634507a9ba999b8d2
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-15 16:52:24 +0300

    chore: added a carousel

--- src/app/features/auth/layout/auth.layout.component.ts
+++ src/app/features/auth/layout/auth.layout.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarouselModule],
   templateUrl: './auth.layout.component.html',
   styleUrl: './auth.layout.component.scss'
 })
 export class AuthLayoutComponent {
 
+  slides =[
+    {
+      image: 'assets/img/businessman-working-laptop 1.png',
+      quote: 'It simplifies collaboration and keeps everyone on the same page, allowing us to focus more on creativity and less on logistics.',
+      author: 'Robert Howard - Project Manager'
+    },
+    {
+      image: 'assets/img/happyman.png',
+      quote: 'It simplifies collaboration and keeps everyone on the same page, allowing us to focus more on creativity and less on logistics.',
+      author: 'Robert Howard - Project Manager'
+    },
+    {
+      image: 'assets/img/happy-team.png',
+      quote: 'It simplifies collaboration and keeps everyone on the same page, allowing us to focus more on creativity and less on logistics.',
+      author: 'Robert Howard - Project Manager'
+    }
+
+  ]
 }
