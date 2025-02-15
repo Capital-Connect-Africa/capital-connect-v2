fe063917befb45c2ca660db81d1ee03ce5dc5f86
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-16 01:53:12 +0300

    chore: rernder the toast to the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; 
+import { ToastComponent } from "./core/components/popups/toast/toast.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
