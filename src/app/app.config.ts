7a2abe3e0d9abea2eec6eee228eb8562708a2bc4
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-15 17:38:26 +0300

    chore: added an animation on signup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
-
 import { routes } from './app.routes';
-import { FilterMatchMode } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/app/features/auth/pages/signup/signup.component.ts
+++ src/app/features/auth/pages/signup/signup.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { SelectButtonComponent } from "../../../../core/components/select-button/select-button.component";
 
 @Component({
   selector: 'app-signup',
   imports: [RouterLink, SelectButtonComponent],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate('1s ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class SignupComponent {
 
+export class SignupComponent {
+  isTyping = false;
+  animationState = true;
+
+  startTyping() {
+    this.isTyping = true;
+  }
 }
