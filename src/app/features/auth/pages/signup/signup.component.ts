30c9ea7a85595186f0471d41df15f09a9ec10189
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-15 15:06:19 +0300

    chore: added user selection buttons

--- src/app/features/auth/pages/signup/signup.component.ts
+++ src/app/features/auth/pages/signup/signup.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SelectButtonComponent } from "../../../../core/components/select-button/select-button.component";
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [RouterLink, SelectButtonComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
