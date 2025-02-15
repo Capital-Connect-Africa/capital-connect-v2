810ff2e3b6f6559463046f6dbf0a1b391e50dc93
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-16 01:52:36 +0300

    chore: added the message and confirm services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,22 @@
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeng/themes/lara';
 import { routes } from './app.routes';
+import Lara from '@primeng/themes/lara';
+import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { HttpErrorInterceptor } from './core/interceptors/http.error.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    MessageService, ConfirmationService,
+    provideHttpClient(withInterceptors([LoadingInterceptor, AuthInterceptor, HttpErrorInterceptor])),
     providePrimeNG({
       ripple: true,
       theme: {
@@ -19,8 +26,8 @@ export const appConfig: ApplicationConfig = {
           darkModeSelector: 'light',
           cssLayer: {
             name: 'primeng',
-            order: 'tailwind-base, primeng, tailwind-utilities'
-        }
+            order: 'tailwind-base, primeng, tailwind-utilities',
+          },
         },
       },
     }),
