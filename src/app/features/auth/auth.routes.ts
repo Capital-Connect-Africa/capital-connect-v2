c02c0cc2cdbc7252413395035e0117d6869141b1
Trevor-crypto243 <otitrevor450@gmail.com>
2025-02-18 12:15:45 +0300

    Auth page

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/signup/signup.component').then(c => c.SignupComponent)},
+  { path: '', loadComponent: () => import('./pages/signin/signin.component').then(c => c.SignInComponent)},
+  { path: 'signup', loadComponent: () => import('./pages/signup/signup.component').then(c => c.SignupComponent)},
+
 ];
 
 @NgModule({
