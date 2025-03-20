8b7a4d78f689d21bf1108d3d5ed6bd4eb0c9cfce
meltus-bwire <meltusbwire10@gmail.com>
2025-03-21 02:53:18 +0300

    chore: implemented a signin method

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,25 +1,27 @@
-import { map } from 'rxjs';
+import { lastValueFrom, map } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { BaseHttpService } from '../../../core/services/http/base.http.service';
 import { User } from '../../users/interfaces/user.interface';
 import { SignInDto } from '../interfaces/signin.dto.interface';
+import { SignUpDto } from '../interfaces/signup.dto.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService extends BaseHttpService<User> {
   BASE_URL = '/auth';
 
-  signup(payload: SignInDto) {
-    return this.create(`${this.BASE_URL}/signup`, payload).pipe(
+  async signUp(payload: SignUpDto) {
+    return lastValueFrom (this.create(`${this.BASE_URL}/signup`, payload).pipe(
       map((res) => {
-        debugger;
+        return res
       })
-    );
+    ));
   }
 
-  signin(){
-
-  }
-  signout(){
-
+  signIn(payload: SignInDto): Promise<{access_token: string}>{
+    return lastValueFrom (this.create(`${this.BASE_URL}/login`, payload).pipe(
+      map((res:any) => {
+        return res as {access_token: string}
+      })
+    ));
   }
 }
