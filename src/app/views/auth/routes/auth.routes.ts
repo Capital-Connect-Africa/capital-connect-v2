4c1810caaf10d10d293dc6fa9e9261b9e10341f3
meltus-bwire <meltusbwire10@gmail.com>
2025-03-22 17:28:47 +0300

    chore: added apath to the verify-email

--- src/app/views/auth/routes/auth.routes.ts
+++ src/app/views/auth/routes/auth.routes.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadComponent: () => import('../pages/signin/signin.component').then(c => c.SignInComponent)},
   { path: 'signup', loadComponent: () => import('../pages/signup/signup.component').then(c => c.SignupComponent)},
+  { path: 'verify-email', loadComponent: () => import('../pages/verify-email/verify-email.component').then(c => c.VerifyEmailComponent)},
 
 ];
 
