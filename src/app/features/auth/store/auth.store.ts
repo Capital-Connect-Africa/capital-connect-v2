6371fe21def6ff1d47e6b27113d920edb8fb8c6b
meltus-bwire <meltusbwire10@gmail.com>
2025-03-22 20:18:04 +0300

    chore: setup session on auth

--- src/app/features/auth/store/auth.store.ts
+++ src/app/features/auth/store/auth.store.ts
@@ -10,11 +10,10 @@ import { SignUpDto } from "../interfaces/signup.dto.interface";
 import { User } from "../../users/interfaces/user.interface";
 import { UsersStore } from "../../users/store/users.store";
 import { ResendEmailVerificationDto } from "../interfaces/resend.verification.email.dto.interface";
+import { Session } from "../../../core/interfaces/session.interface";
 
 const initialState:AuthState ={
-    isLoggedIn: false,
-    user: null,
-    accessToken: null,
+    session: null,
     isLoading: false,
 }
 
@@ -24,18 +23,20 @@ export const AuthStore =signalStore(
     withMethods((store, authservice =inject(AuthService), toast =inject(ToastService), userStore =inject(UsersStore)) =>({
 
         async signIn(credentials: SignInDto){
-            patchState(store, { isLoading: true })
             try {
+                patchState(store, { isLoading: true });
                 const { access_token } =await authservice.signIn(credentials);
-                const { firstName } =this.decodeAccessToken(access_token);
+                const accessTokenPayload =this.decodeAccessToken(access_token);
+                this.setSession({accessTokenPayload, accessToken: access_token});
                 patchState(store, { isLoading: false  });
                 toast.show({
                     summary: 'Login',
-                    details: `Login successful. Welcome back ${firstName}`,
+                    details: `Login successful. Welcome back ${accessTokenPayload.firstName}`,
                     severity: 'success',
                     position: 'bottom-right'
                 })
             } catch (error:any) {
+                debugger
                 patchState(store, {  isLoading: false  })
             }
         },
@@ -44,8 +45,20 @@ export const AuthStore =signalStore(
             return jwtDecode(accessToken) as AccessTokenPayload;
         },
 
+        getSession():Session | null{
+            const session =store.session() ?? JSON.parse(localStorage.getItem('session') || '{}') as Session;
+            userStore.setCurrentUser(session.user as User)
+            userStore.setVerificationEmail(session.user?.username?? '');
+            return session;
+        },
+
+        setSession(session: Session){
+            patchState(store, state =>({ session: {  ...state.session, ...session }}))
+            localStorage.setItem('session', JSON.stringify(store.session()));
+        },
+
         getAccessToken(){
-            return (store.accessToken() ?? localStorage.getItem('accessToken')) ?? ''
+            return this.getSession()?.accessToken;
         },
 
         async signUp(payload: SignUpDto){
