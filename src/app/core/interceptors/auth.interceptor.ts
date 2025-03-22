324ff37eaafbbd43f4d77f3291a113a5d66adead
meltus-bwire <meltusbwire10@gmail.com>
2025-03-22 17:22:28 +0300

    refactor: use the access token from the store

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthStore } from '../../features/auth/store/auth.store';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const authToken = localStorage.getItem('authToken') || '';
+  const store =inject(AuthStore)
+  const authToken = store.getAccessToken();
   const clonedRequest = req.clone({
     setHeaders: {
       Authorization: `Bearer ${authToken}`,
