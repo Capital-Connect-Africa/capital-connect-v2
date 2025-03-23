8bffa76a55326c6191a63f6f183f50bee89a7929
meltus-bwire <meltusbwire10@gmail.com>
2025-03-23 14:36:05 +0300

    chore: added error handling

--- src/app/features/auth/store/auth.store.ts
+++ src/app/features/auth/store/auth.store.ts
@@ -11,6 +11,8 @@ import { User } from "../../users/interfaces/user.interface";
 import { UsersStore } from "../../users/store/users.store";
 import { ResendEmailVerificationDto } from "../interfaces/resend.verification.email.dto.interface";
 import { Session } from "../interfaces/session.interface";
+import { ErrorStore } from "../../../core/store/http.errors.store";
+import { objTostr } from "../../../core/utils/object.to.string.util";
 
 const initialState:AuthState ={
     session: null,
@@ -20,83 +22,108 @@ const initialState:AuthState ={
 export const AuthStore =signalStore(
     {providedIn: 'root'},
     withState(initialState),
-    withMethods((store, authservice =inject(AuthService), toast =inject(ToastService), userStore =inject(UsersStore)) =>({
+    withMethods((store, authservice =inject(AuthService), errorStore =inject(ErrorStore), toast =inject(ToastService), userStore =inject(UsersStore)) =>{
+        return {
+            async signIn(credentials: SignInDto){
+                try {
+                    patchState(store, { isLoading: true });
+                    const { access_token } =await authservice.signIn(credentials);
+                    const accessTokenPayload =this.decodeAccessToken(access_token);
+                    this.setSession({accessTokenPayload, accessToken: access_token});
+                    toast.show({
+                        summary: 'Login',
+                        details: `Login successful. Welcome back ${accessTokenPayload.firstName}`,
+                        severity: 'success',
+                        position: 'bottom-right'
+                    })
+                } catch (e:any) {
+                    const err =errorStore.error();
+                    toast.show({
+                        summary: `${err? 'Login': ''}`,
+                        details: objTostr(err?.message ?? e.message),
+                        severity: 'error',
+                        position: 'bottom-right'
+                    })
+                }finally{
+                    patchState(store, {  isLoading: false  })
+                }
+            },
 
-        async signIn(credentials: SignInDto){
-            try {
+            decodeAccessToken(accessToken:string){
+                return jwtDecode(accessToken) as AccessTokenPayload;
+            },
+
+            getSession():Session | null{
+                const session =store.session() ?? JSON.parse(sessionStorage.getItem('session') || '{}') as Session;
+                userStore.setCurrentUser(session.user as User)
+                userStore.setVerificationEmail(session.user?.username?? '');
+                return session;
+            },
+
+            setSession(session: Session){
+                patchState(store, state =>({ session: {  ...state.session, ...session }}))
+                sessionStorage.setItem('session', JSON.stringify(store.session()));
+            },
+
+            getAccessToken(){
+                return this.getSession()?.accessToken
+            },
+
+            clear(){
+                userStore.clear()
+                patchState(store, {session: null});
+                sessionStorage.removeItem('session');
+            },
+
+            async signUp(payload: SignUpDto){
+                patchState(store, { isLoading: true })
+                try {
+                    const user:User =await authservice.signUp(payload);
+                    userStore.setCurrentUser(user);
+                    userStore.setVerificationEmail(user.username);
+                    toast.show({
+                        summary: 'Register',
+                        details: `Signup successful. Thank you for choosing Capital Connect Africa.`,
+                        severity: 'success',
+                        position: 'bottom-right'
+                    })
+
+                } catch (e:any) {
+                    const err =errorStore.error()
+                    toast.show({
+                        summary: `${err? 'Login': ''}`,
+                        details: objTostr(err?.message ?? e.message),
+                        severity: 'error',
+                        position: 'bottom-right'
+                    })
+                    
+                }finally{
+                    patchState(store, { isLoading: false  })
+                }
+            },
+
+            async resendEmailVerificationToken(payload: ResendEmailVerificationDto){
                 patchState(store, { isLoading: true });
-                const { access_token } =await authservice.signIn(credentials);
-                const accessTokenPayload =this.decodeAccessToken(access_token);
-                this.setSession({accessTokenPayload, accessToken: access_token});
-                patchState(store, { isLoading: false  });
-                toast.show({
-                    summary: 'Login',
-                    details: `Login successful. Welcome back ${accessTokenPayload.firstName}`,
-                    severity: 'success',
-                    position: 'bottom-right'
-                })
-            } catch (error:any) {
-                debugger
-                patchState(store, {  isLoading: false  })
-            }
-        },
+                try {
+                    const { message } =await authservice.resendEmailVerificationToken(payload);
+                    patchState(store, { isLoading: false })
+                    toast.show({
+                        summary: 'Delivery Report',
+                        details: message,
+                        severity: 'success',
+                        position: 'bottom-right'
+                    })
 
-        decodeAccessToken(accessToken:string){
-            return jwtDecode(accessToken) as AccessTokenPayload;
-        },
-
-        getSession():Session | null{
-            const session =store.session() ?? JSON.parse(localStorage.getItem('session') || '{}') as Session;
-            userStore.setCurrentUser(session.user as User)
-            userStore.setVerificationEmail(session.user?.username?? '');
-            return session;
-        },
-
-        setSession(session: Session){
-            patchState(store, state =>({ session: {  ...state.session, ...session }}))
-            localStorage.setItem('session', JSON.stringify(store.session()));
-        },
-
-        getAccessToken(){
-            return this.getSession()?.accessToken;
-        },
-
-        async signUp(payload: SignUpDto){
-            patchState(store, { isLoading: true })
-            try {
-                const user:User =await authservice.signUp(payload);
-                patchState(store, { isLoading: false })
-                userStore.setCurrentUser(user);
-                userStore.setVerificationEmail(user.username);
-                toast.show({
-                    summary: 'Register',
-                    details: `Signup successful. Thank you for choosing Capital Connect Africa.`,
-                    severity: 'success',
-                    position: 'bottom-right'
-                })
-
-            } catch (error:any) {
-                patchState(store, {
-                    isLoading: false
-                })
-            }
-        },
-
-        async resendEmailVerificationToken(payload: ResendEmailVerificationDto){
-            patchState(store, { isLoading: true });
-            try {
-                const { message } =await authservice.resendEmailVerificationToken(payload);
-                patchState(store, { isLoading: false })
-                toast.show({
-                    summary: 'Delivery Report',
-                    details: message,
-                    severity: 'success',
-                    position: 'bottom-right'
-                })
-
-            } catch (error) {
-                patchState(store, { isLoading: false })
+                } catch (error) {
+                    patchState(store, { isLoading: false })
+                }
+            },
+            signOut(){
+                this.clear()
+                userStore.clear()
             }
         }
-    }))
+
+
+    })
 )
