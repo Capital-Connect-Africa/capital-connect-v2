133b824369569e8dd05b34acadf022464abb8133
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-04 20:56:18 +0300

    chore: added route to onboarding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './views/auth/components/layout/auth.layout.component';
 import { PublicLayoutComponent } from './views/public/components/public-layout/public-layout.component';
+import { OnboardingLayoutComponent } from './views/onboarding/components/layout/onboarding-layout/onboarding-layout.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'auth', 
     component: AuthLayoutComponent,
     loadChildren: () => import('./views/auth/routes/auth.routes').then((m) => m.AuthRoutes),
+  },
+  {
+    path: 'onboarding', 
+    component: OnboardingLayoutComponent,
+    loadChildren: () => import('./views/onboarding/routes/onboarding.routes').then((m) => m.OnboardingRoutes),
   },
 ];
