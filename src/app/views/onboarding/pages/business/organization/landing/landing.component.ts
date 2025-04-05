3f96a645cafc9e02d1a98854e12fc77ae038467f
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-05 21:49:01 +0300

    feat: added a landing page

---
+++ src/app/views/onboarding/pages/business/organization/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../../../../components/button/button.component";
+
+@Component({
+  selector: 'app-landing',
+  imports: [ButtonComponent],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+
+}
