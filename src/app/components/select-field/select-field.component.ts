0f2423376fcf50a8e4164d862fe5ee93046c05ff
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-06 17:50:02 +0300

    chore: modified select field to work with form

--- src/app/components/select-field/select-field.component.ts
+++ src/app/components/select-field/select-field.component.ts
@@ -1,39 +1,65 @@
-import { Component, Input } from '@angular/core';
-import { SelectModule } from 'primeng/select';
+import { Component, EventEmitter, forwardRef, input, Input, Output, SimpleChanges } from '@angular/core';
+import { SelectChangeEvent, SelectModule } from 'primeng/select';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-interface City {
-  name: string;
-  code: string;
-}
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'select-field',
   imports: [CommonModule, SelectModule, MultiSelectModule, FormsModule],
   templateUrl: './select-field.component.html',
-  styleUrl: './select-field.component.scss'
+  styleUrl: './select-field.component.scss',
+  providers: [
+      {
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => SelectFieldComponent),
+        multi: true,
+      },
+    ],
 })
 
-export class SelectFieldComponent {
+export class SelectFieldComponent implements ControlValueAccessor {
+  @Input() disabled =false;
   @Input() label: string ='';
+  @Input() options:any[] =[];
   @Input() className: string ='';
   @Input() placeholder: string ='';
   @Input() labelClassName: string ='';
-  @Input() type: 'multiple' | 'single' ='multiple';
+  @Input() formControlName: string ='';
+  @Input() type: 'multiple' | 'single' ='single';
+  @Input() optionLabel: string | undefined =undefined;
+  @Input() optionValue: string | undefined =undefined;
+  @Output() valueChange = new EventEmitter<any>();
 
-  cities: City[] | undefined;
-
-    selectedCity: City | undefined;
-
-    ngOnInit() {
-        this.cities = [
-            { name: 'New York', code: 'NY' },
-            { name: 'Rome', code: 'RM' },
-            { name: 'London', code: 'LDN' },
-            { name: 'Istanbul', code: 'IST' },
-            { name: 'Paris', code: 'PRS' }
-        ];
+    value: any;
+    onChange = (value: string) => {};
+    onTouched = () => {};
+    
+    
+    writeValue(value: string): void {
+      this.value = value ?? '';
+    }
+  
+    registerOnChange(fn: (value: string) => void): void {
+      this.onChange = fn;
+    }
+  
+    registerOnTouched(fn: () => void): void {
+      this.onTouched = fn;
+    }
+    
+    onInputChange(event: SelectChangeEvent): void {
+      this.value = event.value;
+      this.onChange(this.value);
+      this.onTouched();
+    }
+  
+    ngOnChanges(changes: SimpleChanges): void {
+      if (changes['type']) {
+        this.type = changes['type'].currentValue;
+      }
+      if (changes['disabled']) {
+        this.disabled = changes['disabled'].currentValue;
+      }
     }
 }
