ca7f277e8a43718e482f67550ec5ca1c3ceb2b7f
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-06 22:21:45 +0300

    chore: added a success page

---
+++ src/app/views/onboarding/pages/business/organization/success/success.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../../../components/button/button.component';
+
+@Component({
+  selector: 'app-success',
+  imports: [ButtonComponent],
+  templateUrl: './success.component.html',
+  styleUrl: './success.component.scss'
+})
+export class SuccessComponent {
+
+}
