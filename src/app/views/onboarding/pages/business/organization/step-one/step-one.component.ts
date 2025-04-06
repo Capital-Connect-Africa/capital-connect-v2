b578bf62fd6caeeecf787d93f353ad88f546cb12
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-06 17:49:23 +0300

    chore: added select fields

--- src/app/views/onboarding/pages/business/organization/step-one/step-one.component.ts
+++ src/app/views/onboarding/pages/business/organization/step-one/step-one.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SelectFieldComponent } from "../../../../../../components/select-field/select-field.component";
 import { ButtonComponent } from "../../../../../../components/button/button.component";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'organization-setup-step-one',
-  imports: [SelectFieldComponent, ButtonComponent],
+  imports: [SelectFieldComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './step-one.component.html',
   styleUrl: './step-one.component.scss'
 })
 export class StepOneComponent {
+  formBuilder =inject(FormBuilder);
 
+  formGroup =this.formBuilder.group({
+    name: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    sector: ['', [Validators.required]],
+    subsector: ['', [Validators.required]],
+    segment: ['', [Validators.required]],
+    products: ['', [Validators.required]],
+  })
+
+  handleSubmit(){
+    const values =this.formGroup.value
+  }
 }
